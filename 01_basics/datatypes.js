a1b2210af1b40bff98207e335161d55e9e55b66c
AmritaNair37 <127662043+AmritaNair37@users.noreply.github.com>
2024-03-09 10:53:14 +0000

    datatypes

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,12 @@
+"use strict";
+//alert(3 + 3)
+let name="nana" //string
+let age=23    //number
+let truth=false  //bool
+//bigint
+//null standalone val
+//undefined
+//symbol => unique
+//object
+console.log(typeof null);
+console.log(typeof undefined)
---
+++ 01_basics/variables.js
@@ -0,0 +1,10 @@
+const accoundId=133
+let accountEmail="amritanair37@gmail.com"
+var accountPassword="12345"
+accountCity="jaipur"
+//accoundId=138
+console.log(accoundId);
+accountEmail="amrita@gmail.com"
+accountCity="punjab"
+accountPassword=123
+console.table([accoundId,accountEmail,accountPassword,accountCity])

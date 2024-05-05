b151a7c81ed6ab1dbe0017f826920bc63184ebbb
16Amrita <142768761+16Amrita@users.noreply.github.com>
2024-05-05 07:55:44 +0000

    done

--- 01_basics/datatypes.js
+++ 01_basics/datatypes.js
@@ -1,12 +1,12 @@
-"use strict";
-//alert(3 + 3)
+"use strict";  //treats all JS code as newer version and not necessary to write nowadays since its done automatically
+//alert(3 + 3)  //this is done in dev console(browser) theres diff syntax for node
 let name="nana" //string
-let age=23    //number
+let age=23    //number 2 to power 53
 let truth=false  //bool
 //bigint
 //null standalone val
 //undefined
 //symbol => unique
 //object
-console.log(typeof null);
-console.log(typeof undefined)
+console.log(typeof null); //object is the value
+console.log(typeof undefined) //undefined is the value
--- 01_basics/variables.js
+++ 01_basics/variables.js
@@ -2,9 +2,17 @@ const accoundId=133
 let accountEmail="amritanair37@gmail.com"
 var accountPassword="12345"
 accountCity="jaipur"
-//accoundId=138
+//accoundId=138 //not allowed since const me declared hai 
 console.log(accoundId);
 accountEmail="amrita@gmail.com"
 accountCity="punjab"
 accountPassword=123
 console.table([accoundId,accountEmail,accountPassword,accountCity])
+
+//let const var mile tho memory me assign karo
+//let and var me diff
+//scoping problem in var that is {   }
+//var me ek value change kiya and it is used in multiple files me use kiya 
+//rahega tho sabme change hogaa
+//prefer not to use var due to function and block scope
+//if variable sirf declare karke chod diya tho print karenge tho value undefined aayega

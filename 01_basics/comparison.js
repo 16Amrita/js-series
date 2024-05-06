0b4345b423f0fdad7f3b5517580400cfc06590b5
16Amrita <142768761+16Amrita@users.noreply.github.com>
2024-05-06 14:01:44 +0000

    hh

---
+++ 01_basics/comparison.js
@@ -0,0 +1,6 @@
+console.log(2>1)
+//make sure that the comparsion is between same datatypes
+//bcs sometimes unpredictable outputs
+console.log("2" > 1)
+console.log("02"<1)
+//here it automatically converted to number
